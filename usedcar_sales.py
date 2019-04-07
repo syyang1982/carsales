ba19e3a0d6dee7668b127bc39982b9f09e63f01b
syyang1982 <syyang2007@gmail.com>
2019-04-07 20:46:01 +1000

    Able to fetch data from carsales.com and plot scatter with markersize

--- usedcar_sales.py
+++ usedcar_sales.py
@@ -53,7 +53,8 @@ def parsehtmltoDataFrame(txt):
     cardata['transmission'] = transmission
     cardata['price'] = pricelist
     cardata['price_type'] = pricetypelist
-    cardata['year'] = [(int(a.lstrip().split(" ")[0])-2011)*8 for a in vehtitle]
+    cardata['year'] = [(int(a.lstrip().split(" ")[0])) for a in vehtitle]
+    # cardata['year'] = [(int(a.lstrip().split(" ")[0])-2011)*8 for a in vehtitle]
     return pd.DataFrame(cardata)
 
 
@@ -131,15 +132,11 @@ TODO:
 '''
 if __name__ == "__main__":
     starttime = datetime.datetime.now()
-    # URL = "https://www.carsales.com.au/cars/results/?q=%28And.Service.CARSALES._.Seats.range%287..%29._.%28C.State.Victoria._.Region.Melbourne.%29_.Doors.5._.SiloType.Dealer%20used%20cars._.Year.range%282010..2012%29._.%28C.Make.Toyota._.Model.Kluger.%29%29&sortby=TopDeal&limit=50"
-    # URL = "https://www.carsales.com.au/cars/dealer/private/demo/toyota/kluger/grande-badge/gsu55r-series/victoria-state/3008-10km-postcode/automatic-transmission/?WT.z_srchsrcx=makemodel"
 
     # fake request as a browser to avoid block
-    # headers = {
-    #     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
     comment = "kluger"
-    url_kluger = "https://www.carsales.com.au/cars/results/?sortby=TopDeal&limit=30&q=(And.Service.CARSALES._.(C.Make.Toyota._.Model.Kluger.)_.(Or.SiloType.Dealer+used+cars._.SiloType.Private+seller+cars._.SiloType.Demo+and+near+new+cars.)_.(C.State.Victoria._.Region.Melbourne.)_.GenericGearType.Automatic._.FuelType.Petrol+-+Unleaded+ULP._.BodyStyle.SUV._.Price.range(..25000)._.Year.range(2012..2016).)"
+    url_kluger = "https://www.carsales.com.au/cars/dealer/toyota/kluger/victoria-state/melbourne-region/suv-bodystyle/automatic-transmission/petrol-unleaded-ulp-fueltype/?limit=40&setype=pagination&sortby=TopDeal"
+
     headers = {
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
@@ -152,15 +149,18 @@ if __name__ == "__main__":
         print(df.year)
 
         filename = input("please key in file name: ")
-        # filename = "first"
+        year = [df.year]
+        year = [(a-2011)*2 for a in year]
         makescatterplot(df['price'],
                         df['kms'],
-                        'Price in AUD',
-                        'Kms',
+                        'price in AUD',
+                        'kms',
                         'Car mileage and price scatter ' + comment,
                         500,
                         filename,
-                        markersize=list(df.year))
+                        markersize=year)
+
+        # df.to_csv('cardata.csv')
 
         stoptime = datetime.datetime.now()
         print('plot finished in {0} seconds'.format(stoptime - starttime))
